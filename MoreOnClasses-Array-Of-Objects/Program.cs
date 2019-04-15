f5c013c253854faac0683a20f4fd0837905448e0
Tomasz Cieslar <tomasz.cieslar@capgemini.com>
2019-04-15 21:05:26 +0200

    final solution

--- MoreOnClasses-Array-Of-Objects/Program.cs
+++ MoreOnClasses-Array-Of-Objects/Program.cs
@@ -6,7 +6,21 @@ namespace MoreOnClasses_Array_Of_Objects
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Create a class named "Table". It must have a constructor, indicating the width and height of the board.
+            //It will have a method "ShowData" which will write on the screen the width and that height of the table. 
+            //Create an array containing 10 tables, with random sizes between 50 and 200 cm, and display all the data.
+
+            Table[] ArrayOfTables = new Table[10];
+
+            for (int i = 0; i < ArrayOfTables.Length; i++)
+            {
+                Random rnd = new Random();
+                ArrayOfTables[i] = new Table(rnd.Next(50, 201), rnd.Next(50, 201));
+                ArrayOfTables[i].ShowData();
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
---
+++ MoreOnClasses-Array-Of-Objects/Table.cs
@@ -0,0 +1,28 @@
+﻿namespace MoreOnClasses_Array_Of_Objects
+{
+    internal class Table
+    {
+        private float width;
+        private float height;
+
+        public Table()
+        {
+
+        }
+
+        public Table(float w, float h)
+        {
+            this.width = w;
+            this.height = h;
+        }
+
+        public float Width { get; set; }
+        public float Height { get; set; }
+
+
+        public void ShowData()
+        {
+            System.Console.WriteLine("Width: {0}, Height {1}", width, height);
+        }
+    }
+}
